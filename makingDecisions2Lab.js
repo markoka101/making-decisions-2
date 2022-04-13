8643f6637561eca0265baec89c35c151851c0c2b
markoka101 <markoka101@gmail.com>
2022-04-13 16:58:12 -0400

    completed all the problemns of the  lab

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -156,12 +156,33 @@ If the letter grade is not one of the above letters, console.log 'Not an eligibl
 */
 
 //Code Here
-
+switch(letterGrade) {
+  case 'A':
+    console.log("The student is doing excellently");
+    break;
+  case 'B':
+    console.log("The student is doing well");
+    break;
+  case 'C':
+    console.log("The student us doing alright");
+    break;
+  case 'D':
+    console.log("The student is not doing very well");
+    break;
+  case 'F':
+    console.log("The student is failing");
+    break;
+  default:
+    console.log("Not an eligible grade");
+    break;
+}
 
 
 ////////// PROBLEM 11 //////////
 /* The famous FizzBuzz, Devmountain style!
-  Create a for loop that iterates from 1 to 100. In this for loop, using some conditional logic, if the number your for loop is currently on is divisible by 3, console.log 'Dev'. If the number is divisible by 5, console.log 'mountain'. If the number is divisible by 5 & 3, console.log 'Devmountain'. If the number is not divisible by 5 or 3, console.log the number itself. Hint: Look up the modulo operator.
+  Create a for loop that iterates from 1 to 100. In this for loop, using some conditional logic, if the number your for loop is currently on is divisible by 3, console.log 'Dev'. 
+  If the number is divisible by 5, console.log 'mountain'. If the number is divisible by 5 & 3, console.log 'Devmountain'. 
+  If the number is not divisible by 5 or 3, console.log the number itself. Hint: Look up the modulo operator.
 
   Your output should look like:
   1
@@ -184,3 +205,14 @@ If the letter grade is not one of the above letters, console.log 'Not an eligibl
 */
 
 //Code Here
+for (let i = 1; i <= 100; i++) {
+  if (i % 5 === 0 && i % 3 === 0) {
+    console.log("Devmountain");
+  } else if (i % 3 === 0) {
+    console.log("Dev");
+  } else if (i % 5 === 0) {
+    console.log("mountain");
+  } else {
+    console.log(i);
+  }
+}
